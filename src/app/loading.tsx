2c817c5972887bfde5f77da72ac5c9655eda7c99
Hyleax <normanyaptw@yahoo.com>
2024-04-01 16:36:59 +1000

    added all functionality, except for adding new member

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center'>
+        <h1 className='text-4xl'>loading...</h1>
+    </div>
+  )
+}
+
+export default loading
